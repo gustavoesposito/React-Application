461eb151fbf3f2b044ef8e2bb13ca821c9c77a6b
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-07-12 07:16:31 -0300

    att

--- costs/src/App.js
+++ costs/src/App.js
@@ -3,6 +3,7 @@ import Home from './pages/Home.js';
 import Empresa from './pages/Empresa.js';
 import Contato from './pages/Contato.js';
 import Navbar from './components/Navbar';
+import Footer from './components/layout/Footer'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
             <Route path='/empresa' element={<Empresa/>}></Route>
             <Route path='/contato' element={<Contato/>}></Route>
           </Routes>
+          <Footer/>
         </Router>
 
       </header>
