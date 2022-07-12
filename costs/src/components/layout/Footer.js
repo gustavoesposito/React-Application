ea89df4054b99eba7aba071fb72df3e32dcf1780
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-07-12 07:31:53 -0300

    att

--- costs/src/components/layout/Footer.js
+++ costs/src/components/layout/Footer.js
@@ -4,7 +4,9 @@ import {FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
 function Footer (){
     return <footer>
         <ul>
-            
+            <li><FaFacebook/></li>
+            <li><FaInstagram/></li>
+            <li><FaLinkedin/></li>
         </ul>
       
          </footer>
