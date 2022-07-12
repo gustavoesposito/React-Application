92d08f88c48c0d912df36a2d913c166cb6ea6c1c
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-07-12 11:53:15 -0300

    att

--- costs/src/components/Navbar.js
+++ costs/src/components/Navbar.js
@@ -1,23 +1,33 @@
 import { Link } from "react-router-dom"
 
+import Container from "./layout/Container"
+
 import styles from './Navbar.module.css'
+import logo from '../img/costs_logo.png'
 
 function Navbar(){
     return(
-        <ul className={styles.list}>
-        <li className={styles.item}>
-          <Link to='/'>Home</Link>
-        </li>
-        <li className={styles.item}>
-          <Link to='/empresa'>Sobre</Link>
-        </li>
-        <li className={styles.item}>
-          <Link to='/contato'>Contato</Link>
-        </li>
-        <li className={styles.item}>
-          <Link to='/newproject'>Novo Projeto</Link>
-        </li>
-      </ul>
+        <nav>
+          <Container>
+            <Link to="/">
+              <img src={logo} alt = "Costs Logo"/>
+            </Link>
+            <ul >
+            <li >
+              <Link to='/'>Home</Link>
+            </li>
+            <li >
+              <Link to='/empresa'>Sobre</Link>
+            </li>
+            <li>
+              <Link to='/contato'>Contato</Link>
+            </li>
+            <li >
+              <Link to='/newproject'>Novo Projeto</Link>
+            </li>
+                  </ul>
+          </Container>
+        </nav>
     )
 }
 
