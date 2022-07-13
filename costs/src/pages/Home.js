aa5669f52171ecbb6403b9e1ef2773aea869f993
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-07-13 10:44:57 -0300

    att

--- costs/src/pages/Home.js
+++ costs/src/pages/Home.js
@@ -1,12 +1,13 @@
 import styles from './Home.module.css'
 import savings from '../img/economia.svg'
+import LinkButton from '../components/layout/LinkButton'
 
 function Home(){
     return(
        <section className={styles.home_container}>
         <h1>Bem-Vindo ao <span>Costs</span></h1>
         <p>Comece agora mesmo a gerenciar seus projetos</p>
-        <a href="/">Criar Projeto</a>
+       <LinkButton to="newproject" text="Criar Projeto"/>
         <img src={savings} alt="Costs"></img>
        </section>
     )
