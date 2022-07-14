08f37fb233f95cbd7bad15f10dc7d77bd89178de
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-07-14 07:37:47 -0300

    att

--- costs/src/components/projects/ProjectForm.js
+++ costs/src/components/projects/ProjectForm.js
@@ -1,11 +1,13 @@
+import styles from './ProjectForm.module.css'
+
 function ProjectForm(){
 return (
-    <form>
+    <form className={styles.form}>
         <div>
             <input type="text" placeholder="Insira o nome do projeto"/>
         </div>
         <div>
-            <input type="number" placeholder="Insira o preço tota"/>
+            <input type="number" placeholder="Insira o preço total"/>
             </div>
         <div>
             <select name="category_id">
