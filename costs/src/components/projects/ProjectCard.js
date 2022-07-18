936cc3e076c659bb5f21dfc88f6b4d75a70448dc
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-07-18 12:41:40 -0300

    att

--- costs/src/components/projects/ProjectCard.js
+++ costs/src/components/projects/ProjectCard.js
@@ -4,9 +4,21 @@ import {BsPencil, BsFillTrashFill} from 'react-icons/bs'
 
 function ProjectCard({id, name, budget, category, handleRemove}){
 return(
-    <p>
-        {name}
+     <div>
+        <h4>{name}</h4>
+        <p>
+            <span>Orçamento:</span> R${budget}
         </p>
+        <p>
+            <span> </span>  {category} 
+        </p>
+        <div>
+            <p>Editar</p>
+            <p>Remover</p>
+        </div>
+
+     </div>
+         
 )
 }
 
--- costs/src/pages/Projects.js
+++ costs/src/pages/Projects.js
@@ -44,7 +44,14 @@ function Projects(){
           <Container customClass="start">
             {projects.length > 0 &&
             projects.map((project)=>
-                <ProjectCard name ={project.name}/>
+                <ProjectCard 
+                name={project.name}
+                id= {project.id}
+                budget={project.budget}
+                category={project.category?project.category.name:project.category}
+                key={project.id}
+                
+                />
             )}
            </Container>
           </div>
