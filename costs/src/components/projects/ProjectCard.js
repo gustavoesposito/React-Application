6472174e1fb5850c00a2d7e33f9cd45f4ae8aaf5
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-07-19 08:14:50 -0300

    att

--- costs/src/components/projects/ProjectCard.js
+++ costs/src/components/projects/ProjectCard.js
@@ -4,7 +4,7 @@ import {BsPencil, BsFillTrashFill} from 'react-icons/bs'
 
 function ProjectCard({id, name, budget, category, handleRemove}){
 return(
-     <div>
+     <div className={styles.project_card}>
         <h4>{name}</h4>
         <p>
             <span>Orçamento:</span> R${budget}
