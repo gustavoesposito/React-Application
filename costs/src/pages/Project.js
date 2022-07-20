6b6f0f0d7b0746f3c2c5ffe7713eeb2c656ba499
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-07-20 20:25:08 -0300

    att

--- costs/src/pages/Project.js
+++ costs/src/pages/Project.js
@@ -10,8 +10,15 @@ const {project,setProject}= useState([])
   
 useEffect(() => {
 
-fetch (`http://localhost:5000/projects/${id}`)//request,rota
+fetch (`http://localhost:5000/projects/${id}`,{
 
+method:'GET',
+headers:
+{
+    'Content-Type': 'application/json',
+},
+
+})//request,rota
 }, [id])//parâmetro importantíssimo que é nossa referência
 
 return     <p>Projeto</p>
