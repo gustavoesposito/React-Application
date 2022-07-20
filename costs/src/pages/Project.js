46132d085479e68ccc6ee2a5c0b2352b830a2326
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-07-20 20:10:25 -0300

    att

--- costs/src/pages/Project.js
+++ costs/src/pages/Project.js
@@ -1,9 +1,14 @@
 import styles from'./Project.module.css'
 
+import{useParams} from 'react-router-dom'//hook para resgatar o id  que vem pela URL, ele serve para pegar os parâmetros
+import {useState,useEffect} from 'react'//controle de estados, e resgatando apenas uma vez
+
 function Project(){
+const {id} =useParams()
 
+const {project,setProject}= useState([])
+  
 
-    
 return     <p>Projeto</p>
 }
 
