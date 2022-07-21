ad3ed04d551d75a675dd66a52077c29eff4a5756
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-07-21 09:57:11 -0300

    att

--- costs/src/pages/Project.js
+++ costs/src/pages/Project.js
@@ -2,6 +2,7 @@ import styles from'./Project.module.css'
 
 import{useParams} from 'react-router-dom'//hook para resgatar o id  que vem pela URL, ele serve para pegar os parâmetros
 import {useState,useEffect} from 'react'//controle de estados, e resgatando apenas uma vez
+import Loading from '../components/layout/Loading'
 
 function Project(){
 const {id} =useParams()
@@ -9,25 +10,19 @@ const {id} =useParams()
 const {project,setProject}= useState([])
   
 useEffect(() => {
-
-fetch (`http://localhost:5000/projects  / ${id}`,{
-
-method:'GET',
-headers:
-{
-    'Content-Type': 'application/json',
-},
-
-})//request,rota
-.then(resp => resp.json())
-.then((data) => {
-    setProject(data)
+setTimeout(() => {
+    
 })
-.catch(err => console.log(err))
+ 
 }, [id])//parâmetro importantíssimo que é nossa referência
 
 return(
     <>
+    {project.name? (
+          <p>{project.name}</p>
+    ):(
+        <Loading/>
+    )}
     </>
 
 )
