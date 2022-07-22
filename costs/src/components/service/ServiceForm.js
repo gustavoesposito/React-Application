eb41c5d1b532d669dc22f726fa4574b8921154b2
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-07-22 10:39:05 -0300

    att

--- costs/src/components/service/ServiceForm.js
+++ costs/src/components/service/ServiceForm.js
@@ -1,7 +1,11 @@
 import styles from  '../projects/ProjectForm.module.css'
 
 function ServiceForm (){
-
+ return(
+    <form action="">
+        <input type="text"/>
+    </form>
+ )
 }
 
 export default ServiceForm
