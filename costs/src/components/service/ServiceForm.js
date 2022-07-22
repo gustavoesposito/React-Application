311fb4f522568c174de58970b5d7adc5bb3cd75f
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-07-22 10:43:11 -0300

    att

--- costs/src/components/service/ServiceForm.js
+++ costs/src/components/service/ServiceForm.js
@@ -1,9 +1,29 @@
 import styles from  '../projects/ProjectForm.module.css'
 
+import Input from '../form/input'
+import SubmitButton from '../form/SubmitButton'
+
+import {useState} from 'react'
+
 function ServiceForm (){
+
+    function submit(){
+
+    }
+    function handleChange(){
+        
+    }
+
  return(
-    <form action="">
-        <input type="text"/>
+    <form onSubmit={submit} className={styles.form}>
+        <Input
+        type="text"
+        text="Nome do serviço"
+        name="name"
+        placeholder="Insira o nome do serviço"
+        handleOnChange={handleChange}
+        
+        />
     </form>
  )
 }
