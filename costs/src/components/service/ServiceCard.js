43d7ab0ffea74153ddf7fff8f588c2f321ac23fe
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-07-22 11:43:45 -0300

    att

--- costs/src/components/service/ServiceCard.js
+++ costs/src/components/service/ServiceCard.js
@@ -1,5 +1,9 @@
-function ServiceCard(){
+import styles from '../projects/ProjectForm.module.css'
 
+function ServiceCard({id, name, cost, description, handleRemove }){
+    <div className={styles.project_card}>
+       <h4>{name}</h4>
+    </div>
 }
 
 export default ServiceCard
